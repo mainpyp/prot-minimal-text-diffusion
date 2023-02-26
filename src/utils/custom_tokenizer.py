ad666422a238860b2e1fa3cdf53e0f399438c59a
Adrian <61416203+mainpyp@users.noreply.github.com>
2023-02-26 13:31:47 +0100

    Update custom_tokenizer.py

--- src/utils/custom_tokenizer.py
+++ src/utils/custom_tokenizer.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 def create_tokenizer(return_pretokenized, path, tokenizer_type: str = "word-level"):
     if return_pretokenized:
         tokenizer = AutoTokenizer.from_pretrained("Rostlab/prot_bert")
-        return tokenizers
+        return tokenizer
 
     if tokenizer_type == "byte-level":
         return read_byte_level(path)
