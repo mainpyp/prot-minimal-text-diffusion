a148e136a1636ca5788a0c3bcc308d2da45d8d3e
mainpyp <henkeladrian@icloud.com>
2023-03-02 15:12:35 +0100

    Adds commands and minimal error file

---
+++ tokenizing_test.py
@@ -0,0 +1,23 @@
+import sys
+from transformers import AutoTokenizer
+import pandas as pd
+
+def main(data_path: str):
+    print("Loading Tokenizer")
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    
+    print("Reading Data to Pandas df")
+    df = pd.read_csv(data_path, sep="\t", header=None)
+    
+    print(df.info())
+    print("Converting data to list")
+    text = df[0].apply(lambda x: x.strip()).tolist()
+    
+    print(sys.getsizeof(text)/8/1_000_000)
+    print("encoding whole dataset")
+    encoded = tokenizer(text)
+
+
+if __name__ == "__main__":
+    data_path = "data/prot_total/prot_total.txt"
+    main(data_path)
