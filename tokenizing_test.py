26f32eefdce1bd349c2300d07b727b88f310a44f
Adrian <61416203+mainpyp@users.noreply.github.com>
2023-03-03 16:50:41 +0100

    Update tokenizing_test.py

--- tokenizing_test.py
+++ tokenizing_test.py
@@ -24,10 +24,10 @@ def preprocess_function(examples):
     inputs = [ex[source_lang] for ex in examples["translation"]]
     targets = [ex[target_lang] for ex in examples["translation"]]
     inputs = [prefix + inp for inp in inputs]
-    model_inputs = tokenizer(inputs, max_length=512, padding=padding, truncation=True)
+    model_inputs = tokenizer(inputs, max_length=args.max_source_length, padding=padding, truncation=True)
 
     # Tokenize targets with the `text_target` keyword argument
-    labels = tokenizer(text_target=targets, max_length=512, padding=padding, truncation=True)
+    labels = tokenizer(text_target=targets, max_length=max_target_length, padding=padding, truncation=True)
 
     # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
     # padding in the loss.
@@ -40,19 +40,38 @@ def preprocess_function(examples):
     return model_inputs
 
 if __name__ == "__main__":
-    data_path = "data/prot_total/prot_total.txt"
-    main(data_path)
-    # from datasets import load_dataset
-    #
-    # data_path = "data/prot_minimal/prot_minimal.txt"
-    #
-    # raw_datasets = load_dataset("csv", data_files=data_path)
-    # print(raw_datasets)
-    # processed_datasets = raw_datasets.map(
-    #     preprocess_function,
-    #     batched=True,
-    #     num_proc=args.preprocessing_num_workers,token
-    #     remove_columns=column_names,
-    #     load_from_cache_file=not args.overwrite_cache,
-    #     desc="Running tokenizer on dataset",
-    # )
+    #data_path = "data/prot_total/prot_total.txt"
+    #path = "data/prot_minimal/prot_minimal.txt"
+    #main(data_path)
+
+
+
+    paths = ["/mnt/home/mheinzinger/deepppi1tb/ProSST5/martins_set/data_mixed/train.csv",
+             "/mnt/home/mheinzinger/deepppi1tb/ProSST5/martins_set/data_mixed/test.csv",
+             "/mnt/home/mheinzinger/deepppi1tb/ProSST5/martins_set/data_mixed/val.csv"]
+
+    print("saved paths")
+
+    from datasets import load_dataset
+    print("load datasets as raw")
+    raw_datasets = load_dataset("csv", data_files=data_files)
+
+    print("process datasets")
+    processed_datasets = raw_datasets.map(
+        preprocess_function,
+        batched=True,
+        num_proc=args.preprocessing_num_workers,
+        remove_columns=column_names,
+        load_from_cache_file=not args.overwrite_cache,
+        desc="Running tokenizer on dataset",
+    )
+
+    print(raw_datasets)
+
+
+
+    
+
+
+
+
