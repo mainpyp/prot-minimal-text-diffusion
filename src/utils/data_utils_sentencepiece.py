79d336d7d76d195f836a59af1b3fd8b20485b280
Adrian <61416203+mainpyp@users.noreply.github.com>
2023-03-03 12:25:07 +0100

    Update data_utils_sentencepiece.py

--- src/utils/data_utils_sentencepiece.py
+++ src/utils/data_utils_sentencepiece.py
@@ -71,8 +71,6 @@ class TextDataset(Dataset):
         
         else:
             print("not encode_batch")
-            import sys
-            self.input_ids = self.tokenizer.encode([t[0] for t in self.text])
             encoded_input = self.tokenizer(self.text)
             self.input_ids = encoded_input["input_ids"]
 
