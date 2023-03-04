4afde84d5b0220ba84aa3c9213eb4b8953a58a0f
Adrian <61416203+mainpyp@users.noreply.github.com>
2023-03-04 14:36:40 +0100

    Update tokenizing_test.py

--- tokenizing_test.py
+++ tokenizing_test.py
@@ -9,6 +9,9 @@ def main(data_path: str):
     
     print("Reading Data to Pandas df")
     df = pd.read_csv(data_path, sep="\t", header=None)
+
+    print(f"df head:\n {df.head()}\n")
+    print(f"df tail:\n {df.tail()}\n")
     
     print(df.info())
     print("Converting data to list")
